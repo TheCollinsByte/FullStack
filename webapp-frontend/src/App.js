70774c72f5b358a8b7fa2fc2d257b65101a2ce23
colspan <collin@collindigm.com>
2023-04-02 23:43:56 +0300

    Frontend: Dependency Initiallization, Throws back a Missing Script

---
+++ webapp-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyApp from './components/MyApp';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Frontend Of the Web Application</h1>
+    </div>
+  );
+}
+
+export default App;
