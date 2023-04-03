75c071cc8f4e79349d2e227195880ce80943d828
colspan <collin@collindigm.com>
2023-04-04 02:44:16 +0300

    Application Content Class Component: Login, Logout and Register arrow functions

--- webapp-frontend/src/components/AppContent.js
+++ webapp-frontend/src/components/AppContent.js
@@ -2,8 +2,57 @@ import * as React from "react";
 import WelcomeContent from "./WelcomeContent";
 import AuthContent from "./AuthContent";
 import LoginForm from "./LoginForm";
+import { request } from "../axios_helper";
 
 export default class AppContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            componentToShow: "Welcome"
+        };
+    };
+
+
+    login = () => {
+        this.setState({componentToShow: "Login"});
+    };
+
+
+    logout = () => {
+        this.setState({componentToShow: "Welcome"});
+    };
+
+    onLogin = (e, username, password) => {
+        e.preventDefault();
+        request(
+            "POST",
+            "/login",
+            { login: username, password: password }
+        ).then((response) => {
+            this.setState({componentToShow: "Messages"});
+        }).catch((error) => {
+            this.setState({componentToShow: "Welcome"});
+        });
+    };
+
+    onRegister = (e, firstName, lastName, username, password) => {
+        e.preventDefault();
+        request(
+            "POST",
+            "/register",
+            { 
+                firstName: firstName,
+                lastName: lastName,
+                login: username, 
+                password: password 
+            }
+        ).then((response) => {
+            this.setState({componentToShow: "Messages"});
+        }).catch((error) => {
+            this.setState({componentToShow: "Welcome"});
+        });
+    };
+
     render() {
         return (
             <div>
