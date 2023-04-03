feaddeed77f5be424b15c83c531b720d77906c86
colspan <collin@collindigm.com>
2023-04-03 14:34:31 +0300

    Helper Function: Defined an axios helper function for the request to the backend application and passed the defaults

---
+++ webapp-frontend/src/axios_helper.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.headers.post["Content-type"] = "application/json";
+
+export const request = (method, url, data) => {
+    return axios({
+        method: method,
+        url: url,
+        data: data
+    });
+};
