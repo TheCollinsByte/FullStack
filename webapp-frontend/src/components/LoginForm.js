cc3ce8e664ed0e809a63c956050e05d23e90cbe5
colspan <collin@collindigm.com>
2023-04-04 02:10:32 +0300

    Class Login Form Component which renders the Login and Registration Form

---
+++ webapp-frontend/src/components/LoginForm.js
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import classNames from 'classnames';
+
+export default class LoginForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: "login",
+            firstName: "",
+            lastName: "",
+            login: "",
+            password: "",
+            onLogin: props.onLogin,
+            onRegister: props.onRegister
+        };
+    }
+
+    onChangeHandler = (event) => {
+        let name = event.target.name;   
+        let value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    onSubmitLogin = (e) => {
+        this.state.onLogin(e, this.state.login, this.state.password);
+    };
+
+    onSubmitRegister = (e) => {
+        this.state.onRegister(
+            e, 
+            this.state.firstName, 
+            this.state.lastName,
+            this.state.login,
+            this.state.password 
+        );
+    };
+
+    render() {
+        return (
+            <div>
+                <div className='row justify-content-center'>
+                    <div className='col-4'>
+                        <ul className='nav nav-pills nav-justified mb-3' id="ex1" rol="tablist">
+                            <li className='nav-item' role='presentation'>
+                                <button 
+                                    className={classNames("nav-link", this.state.active === "login" ? "active" : "")} 
+                                    id='tab-login'>Login</button>
+                            </li>
+                            <li className='nav-item' role='presentation'>
+                                <button 
+                                    className={classNames("nav-link", this.state.active === "register" ? "active" : "")} 
+                                    id='tab-register'>Register</button>
+                            </li>
+                        </ul>
+
+                        <div className='tab-content'>
+                           <div className={classNames("tab-pane", "fade", this.state.active === "login" ? " show active" : "")} id='pills-login'>
+                                <form>
+                                    <div className='form-outline mb-4'>
+                                        <input type='login' id='loginName' name='login' className='form-control'/>
+                                        <label className='form-label' htmlFor='loginName'>Username</label>
+                                    </div>
+
+                                    <div className='form-outline mb-4'>
+                                        <input type='password' id='loginPassword' name='password' className='form-control'/>
+                                        <label className='form-label' htmlFor='loginPassword'>Password</label>
+                                    </div>
+
+                                    <button type='submit' className='btn btn-primary btn-block mb-4'>Sign in</button>
+                                </form>
+                            </div> 
+
+                           <div className={classNames("tab-pane", "fade", this.state.active === "register" ? " show active" : "")} id="pills-register">
+                                <form>
+                                    <div className='form-outline mb-4'>
+                                        <input type='text' id='firstName' name='firstName' className='form-control'/>
+                                        <label className='form-label' htmlFor='firstName'>First Name</label>
+                                    </div>
+
+                                    <div className='form-outline mb-4'>
+                                        <input type='password' id='loginPassword' name='password' className='form-control'/>
+                                        <label className='form-label' htmlFor='loginPassword'>Password</label>
+                                    </div>
+
+                                    <button type='submit' className='btn btn-primary btn-block mb-4'>Sign in</button>
+                                </form>
+                            </div> 
+                            
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
