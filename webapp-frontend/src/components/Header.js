e6b9b8b49ed157c38035ec4dee97dc1f4c8c9363
colspan <collin@collindigm.com>
2023-04-03 08:01:29 +0300

    Defined the class name attributes

--- webapp-frontend/src/components/Header.js
+++ webapp-frontend/src/components/Header.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 export default function Header(props) {
 
     return (
-        <header>
+        <header className='App-header'>
             <img src={props.logoSrc} className="App-logo" alt='Logo' />
-            <h1>{props.pageTitle}</h1>
+            <h1 className='App-title'>{props.pageTitle}</h1>
         </header>
     );
 }
