aff95a9ec56eea8408a057cd480922ecf3789d24
colspan <collin@collindigm.com>
2023-04-03 08:13:55 +0300

    App Content: class Component

---
+++ webapp-frontend/src/components/AppContent.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import WelcomeContent from "./WelcomeContent";
+
+export default class AppContent extends React.Component {
+    render() {
+        return (
+            <div>
+                <WelcomeContent />
+            </div>
+        )
+    };
+}
