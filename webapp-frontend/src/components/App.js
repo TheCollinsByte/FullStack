5866a37ae0f6b6ff63b6c3e9a8b1faf97a5e8533
colspan <collin@collindigm.com>
2023-04-03 08:15:38 +0300

    App content Imported into the Main App Component to be displayed

--- webapp-frontend/src/components/App.js
+++ webapp-frontend/src/components/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import logo from '../logo.png';
 
 import Header from './Header';
+import AppContent from './AppContent';
 
 function App() {
   return (
     <div>
       <Header pageTitle="Frontend authenticated With JWT" logoSrc={logo} />
+
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col'>
+            <AppContent />
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
