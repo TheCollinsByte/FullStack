72936842df050d31a684f8e97d582f2328359654
colspan <collin@collindigm.com>
2023-04-04 14:28:25 +0300

    Exception Handler: Will be executed when an exception is thrown

---
+++ backend-api/src/main/java/com/addaboy/api/config/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.addaboy.api.config;
+
+import com.addaboy.api.dto.ErrorDto;
+import com.addaboy.api.exception.AppException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    /**
+     *  This method will surround every controller but will only execute if and app exception is thrown
+     *
+     */
+
+    @ExceptionHandler(value = {AppException.class})
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(AppException exception) {
+        return ResponseEntity.status(exception.getCode())
+                .body(ErrorDto.builder().message(exception.getMessage()).build());
+    }
+
+}
